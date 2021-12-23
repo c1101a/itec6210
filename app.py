cbb4b72c8dd60fe9ba2b6bd7489d5fbc2888920e
Calin Armenean <calin.armenean@gmail.com>
2021-12-22 21:19:38 -0500

    MAP working

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ ID_marker = re.compile('\.I.')
 
 
 def get_data(PATH_TO_FILE, marker):
-    """
-    Reads the file and splits the text into entries at the ID marker '.I'.
-    The first entry is empty, so it is removed.
-    'marker' contains the regex at which we want to split
-    """
     with open(PATH_TO_FILE, 'r') as f:
         text = f.read().replace('\n', " ")
         lines = re.split(marker, text)
@@ -25,11 +20,40 @@ def get_data(PATH_TO_FILE, marker):
     return lines
 
 
+def is_relevant(q_id, d_id, rel_data):
+    queries = filter(lambda c: c["q_id"] == str(q_id), rel_data)
+    for q in list(queries):
+        if q["d_id"] == d_id:
+            return 1
+    return 0
+
+
+def get_score(doc):
+    return doc.get('score')
+
+
+def rank_docs(q, txt_data, top_n):
+    tokenized_query = q.split(" ")
+    doc_scores = bm25.get_scores(tokenized_query)
+
+    for i in range(len(txt_data)):
+        txt_data[i]['score'] = doc_scores[i]
+
+    txt_data.sort(key=get_score, reverse=True)
+
+    topN = []
+    for doc in txt_data[:top_n]:
+        topN.append(doc)
+
+    return topN
+
+
 txt_list = get_data(PATH_TO_CRAN_TXT, ID_marker)
 qry_list = get_data(PATH_TO_CRAN_QRY, ID_marker)
 
 chunk_start = re.compile('\.[A,B,T,W]')
 txt_data = []
+rel_data = []
 
 for line in txt_list:
     entries = re.split(chunk_start, line)
@@ -40,43 +64,33 @@ for line in txt_list:
     text = entries[4]
 
     txt_data.append({"id": id, "title": title, "text": text, "score": 0})
-    # txt_data[id]['title'] = title
-    # txt_data[id]['author'] = author
-    # txt_data[id]['publication_date'] = publication_date
-    # txt_data[id]['text'] = text
-    # txt_data[id]['score'] = 0
 
+with open(PATH_TO_CRAN_REL, 'r') as f:
+    for line in f:
+        l = line.split(" ")
+        rel_data.append({"q_id": l[0], "d_id": l[1], "rel": l[2]})
 
 tokenized_corpus = [doc['text'].split(" ") for doc in txt_data]
 bm25 = BM25Okapi(tokenized_corpus)
 
 query = "what similarity laws must be obeyed when constructing aeroelastic models of heated high speed aircraft"
-tokenized_query = query.split(" ")
 
-doc_scores = bm25.get_scores(tokenized_query)
+map = 0
+index = 0
+positive = 0
+total_precision = 0
+for i in rank_docs(query, txt_data, 110):
+    index += 1
+    if is_relevant(1, i["id"], rel_data) == 1:
+        positive += 1
+        total_precision += (positive/index)
+print(total_precision/positive)
 
-for i in range(len(txt_data)):
-    txt_data[i]['score'] = doc_scores[i]
-
-
-def get_score(doc):
-    return doc.get('score')
-
-
-txt_data.sort(key=get_score, reverse=True)
-
-top25 = []
-for doc in txt_data[:25]:
-    top25.append(doc)
-
-sia = SentimentIntensityAnalyzer()
-qS = sia.polarity_scores(query)
-qV = [qS['neg'], qS['neu'], qS['pos']]
-for doc in top25:
-    dS = sia.polarity_scores(doc['text'])
-    dV = [dS['neg'], dS['neu'], dS['pos']]
-    print(1 - spatial.distance.cosine(qV, dV))
-
-
-
-# import and parse the evaluation. combine everything and export to excel
+# ## Sentiment
+# sia = SentimentIntensityAnalyzer()
+# qS = sia.polarity_scores(query)
+# qV = [qS['neg'], qS['neu'], qS['pos']]
+# for doc in top25:
+#     dS = sia.polarity_scores(doc['text'])
+#     dV = [dS['neg'], dS['neu'], dS['pos']]
+#     #print(1 - spatial.distance.cosine(qV, dV))
